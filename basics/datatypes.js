3c5d58adfbb22c7dfb41db192a2bd0fc52574965
IAMMDDANISHEQBAL <139456685+IAMMDDANISHEQBAL@users.noreply.github.com>
2024-04-01 18:49:50 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,17 @@
+'use strict'; // older js didnt have classes,module and  arrows were not there but new js have js community didnt want the older code to be unused so they came up with use strict which treats all the codes as new code;
+
+alert(3+3) //we are using nodejs,not browser
+
+//number ==>2 to power 53,
+//bigint
+//string
+//boolean
+//null=>standalone value
+//undefined==>value not defined
+//symbol=to differentiate uniquely
+
+//object 
+
+//---------------------------------------------- output
+console.log(typeof undefined);                  //undefined
+console.log(typeof null);                        // object
